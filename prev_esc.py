be1e96fdf16bf8acdb344fdb1985624392e8c817
Simranjeet Singh <sjsingh9090@gmail.com>
2020-06-29 06:41:02 +0530

    Basic script completed

---
+++ prev_esc.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+#provide execute permission
+import os
+# scan system and find SUID Binaries
+def scan_sys():
+    os.system("echo `find / -perm -u=s -type f 2>/dev/null` > scan.txt")
+
+#get data
+def get_info():
+    if not os.path.isdir("_gtfobins"):
+        try:
+            os.system("svn export https://github.com/GTFOBins/GTFOBins.github.io/trunk/_gtfobins")
+        except:
+            print("check whether svn is installed or not")
+
+def list_suid():
+    with open("scan.txt") as file:
+        data = file.read()
+    commands = data.split()
+    bins=[]
+    for command in commands:
+        bins.append(command.split('/')[-1])
+    bins= set(bins)
+    return bins
+
+def list_vul_suid(bins):
+    vul=[]
+    files = os.listdir('_gtfobins')
+    for entry in bins:
+        if entry+'.md'in files:
+            print(entry)
+            vul.append(entry)
+    return vul
+
+def get_doc(cmd):
+    with open('_gtfobins/'+cmd+'.md') as file:
+        data = file.read()
+    return data
+
+if __name__ == "__main__":
+    scan_sys()
+    get_info()
+    suid = list_suid()
+    vul_suid = list_vul_suid(suid)
+    for entry in vul_suid:
+        print(get_doc(entry))
